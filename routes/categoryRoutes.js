cb4eba588d5131b1cb9867ce2c7980a8bf6f3851
Believe375 <believe.yawson@outlook.com>
2025-06-10 00:49:41 +0000

    Restructure project: cleaned, renamed, and updated files

--- routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createCategory, getAllCategories } = require('../controllers/categoryController');
-const { verifyAdmin } = require('../middleware/authMiddleware');
-
-router.get('/', getAllCategories);
-router.post('/', verifyAdmin, createCategory);
-
-module.exports = router;
